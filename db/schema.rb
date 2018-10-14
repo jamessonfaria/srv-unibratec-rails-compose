c21103acad71ed22806e21703f0d7a1696baf17b
Jamesson Faria <jamessonjr@gmail.com>
2018-10-14 18:44:42 -0300

    added categoria

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_14_191228) do
+ActiveRecord::Schema.define(version: 2018_10_14_214250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categoria", force: :cascade do |t|
+    t.string "descricao"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "jokes", force: :cascade do |t|
     t.text "body"
